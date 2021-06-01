a97a7cbef81411f429ce33f5765b97fdff402475
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-01 11:58:02 +0300

    creating posts with nofeedback works

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@
 const express = require('express');
 const app = express();
 
-const { mongoDbString } = require('./src/config/config');
+const { mongoDbString } = require('./config/config');
+const Post = require('./models/post');
 
 //susiinstaliuojam mongoose , npm install mongoose
 //isitraukiam mongoose paketa
 const mongoose = require('mongoose');
 //prisijungiam prie duomenu bazes
 
-mongoose.connect(mongoDbString, { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose
+  .connect(mongoDbString, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then((result) => {
+    console.log('connected to mongoose, results:');
+    app.listen(3000);
+  })
+  .catch((err) => console.error(err));
 
 //register view engine
 app.set('view engine', 'ejs');
@@ -24,7 +31,17 @@ app.get('/', function (req, res) {
   });
 });
 
+app.get('/add-post', (req, res) => {
+  //sukuriam nauja posta pagal post.js sukurta modeli
+  const newPost = new Post({
+    title: 'This is Mongoose Db',
+    author: 'John Doe',
+    body: 'blah blah blah this is a long text',
+  });
+  //kad issaugoti duomenu bazeje naudojam .save() metoda
+  newPost.save();
+  res.send('all good');
+});
+
 //404 case , kai vartotojas ivede psl kurio nera
 app.use((req, res) => res.status(404).send('Ops , page not found'));
-
-app.listen(3000);
